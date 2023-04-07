823b90bf2daf8b4d9b03c1ee03b7e5baba2e6bb5
ManhNguyen <manhddt2508@gmail.com>
2023-04-07 08:49:47 +0700

    remove Sidebar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,19 +1,11 @@
 import classNames from 'classnames/bind';
 
 import styles from './Sidebar.module.scss';
-import Menu, { MenuItem } from './Menu';
-import { faHouse, faHouseChimney } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
-  return (
-    <aside className={cx('wrapper')}>
-      <Menu>
-        <MenuItem title={'Home'} icon={faHouse} activeIcon={faHouseChimney}></MenuItem>
-      </Menu>
-    </aside>
-  );
+  return <aside className={cx('wrapper')}></aside>;
 }
 
 export default Sidebar;
