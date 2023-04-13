d0f3e52a2bd0223287a997c93dbd3877b27eb8f8
ManhNguyen <manhddt2508@gmail.com>
2023-04-13 08:02:21 +0700

    doneCalendar

--- client/src/pages/Staff/Staff.js
+++ client/src/pages/Staff/Staff.js
@@ -3,13 +3,19 @@ import classNames from 'classnames/bind';
 import styles from './Staff.module.scss';
 import Sidebar from '~/components/Sidebar/Sidebar';
 import Button from '~/components/Button/Button';
+import Image from '~/components/Image/Image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRectangleList } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 function Staff() {
+  const [month, setMonth] = useState(new Date().getMonth());
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [showMonthPicker, setShowMonthPicker] = useState(false);
+
   const month_names = [
     'January',
     'February',
@@ -33,9 +39,6 @@ function Staff() {
     return isLeapYear(year) ? 29 : 28;
   };
 
-  const [month, setMonth] = useState(new Date().getMonth());
-  const [year, setYear] = useState(new Date().getFullYear());
-
   const generateCalendar = () => {
     const days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
@@ -55,6 +58,8 @@ function Staff() {
   };
 
   const currentDate = new Date();
+  const days = generateCalendar(month, year);
+
   return (
     <div className={cx('wrapper')}>
       <Sidebar />
@@ -89,12 +94,83 @@ function Staff() {
             </Button>
           </div>
         </div>
+        <div className={cx('content')}>
+          <div className={cx('header-content')}>
+            <span className={cx('title-content')}>Đơn hàng gần đây</span>
+            <Link className={cx('all-order')}>Xem toàn bộ đơn hàng</Link>
+          </div>
+          <div className={cx('order-list')}>
+            <div className={cx('order')}>
+              <Image
+                className={cx('order-image')}
+                src="https://dienchau2.edu.vn/wp-content/uploads/2023/03/Meme-My-Dieu-bay-hay-hai-huoc.fna&oh=00_AfDImdqtt7DBEkFehcuqyeDvh6QUGaYyJ3GVCCnnGnNwMA&oe=6403CBA8.jpeg"
+                alt="avatar"
+              ></Image>
+              <div className={cx('name-order')}>Hồ Thanh Hưng</div>
+              <div className={cx('day-order')}>Mon, Dec 26</div>
+              <div className={cx('time-order')}>9.00AM</div>
+              <div className={cx('price-order')}>200.000 đ</div>
+            </div>
+            <div className={cx('order')}>
+              <Image
+                className={cx('order-image')}
+                src="https://dienchau2.edu.vn/wp-content/uploads/2023/03/Meme-My-Dieu-bay-hay-hai-huoc.fna&oh=00_AfDImdqtt7DBEkFehcuqyeDvh6QUGaYyJ3GVCCnnGnNwMA&oe=6403CBA8.jpeg"
+                alt="avatar"
+              ></Image>
+              <div className={cx('name-order')}>Hồ Thanh Hưng</div>
+              <div className={cx('day-order')}>Mon, Dec 26</div>
+              <div className={cx('time-order')}>9.00AM</div>
+              <div className={cx('price-order')}>200.000 đ</div>
+            </div>
+            <div className={cx('order')}>
+              <Image
+                className={cx('order-image')}
+                src="https://dienchau2.edu.vn/wp-content/uploads/2023/03/Meme-My-Dieu-bay-hay-hai-huoc.fna&oh=00_AfDImdqtt7DBEkFehcuqyeDvh6QUGaYyJ3GVCCnnGnNwMA&oe=6403CBA8.jpeg"
+                alt="avatar"
+              ></Image>
+              <div className={cx('name-order')}>Hồ Thanh Hưng</div>
+              <div className={cx('day-order')}>Mon, Dec 26</div>
+              <div className={cx('time-order')}>9.00AM</div>
+              <div className={cx('price-order')}>200.000 đ</div>
+            </div>
+            <div className={cx('order')}>
+              <Image
+                className={cx('order-image')}
+                src="https://dienchau2.edu.vn/wp-content/uploads/2023/03/Meme-My-Dieu-bay-hay-hai-huoc.fna&oh=00_AfDImdqtt7DBEkFehcuqyeDvh6QUGaYyJ3GVCCnnGnNwMA&oe=6403CBA8.jpeg"
+                alt="avatar"
+              ></Image>
+              <div className={cx('name-order')}>Hồ Thanh Hưng</div>
+              <div className={cx('day-order')}>Mon, Dec 26</div>
+              <div className={cx('time-order')}>9.00AM</div>
+              <div className={cx('price-order')}>200.000 đ</div>
+            </div>
+            <div className={cx('order')}>
+              <Image
+                className={cx('order-image')}
+                src="https://dienchau2.edu.vn/wp-content/uploads/2023/03/Meme-My-Dieu-bay-hay-hai-huoc.fna&oh=00_AfDImdqtt7DBEkFehcuqyeDvh6QUGaYyJ3GVCCnnGnNwMA&oe=6403CBA8.jpeg"
+                alt="avatar"
+              ></Image>
+              <div className={cx('name-order')}>Hồ Thanh Hưng</div>
+              <div className={cx('day-order')}>Mon, Dec 26</div>
+              <div className={cx('time-order')}>9.00AM</div>
+              <div className={cx('price-order')}>200.000 đ</div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={cx('calender')}>
+      <div className={cx('calendar')}>
         <div className={cx('calendar-header')}>
-          <span className={cx('month-picker')}>{month_names[month]}</span>
+          <span className={cx('month-picker')} onClick={() => setShowMonthPicker(!showMonthPicker)}>
+            {month_names[month]}
+          </span>
           <div className={cx('year-picker')}>
+            <span className={cx('year-change')} onClick={() => setYear(year - 1)}>
+              &lt;
+            </span>
             <span className={cx('year')}>{year}</span>
+            <span className={cx('year-change')} onClick={() => setYear(year + 1)}>
+              &gt;
+            </span>
           </div>
         </div>
         <div className={cx('calendar-body')}>
@@ -108,19 +184,48 @@ function Staff() {
             <div>Sat</div>
           </div>
           <div className={cx('calendar-days')}>
-            {generateCalendar().map((day, index) => (
+            {days.map((day, index) => (
               <div
                 key={index}
-                className={cx(
-                  'calendar-day',
-                  `${day === new Date().getDate() && month === new Date().getMonth() ? 'curr-date' : ''}`,
-                )}
+                className={
+                  day === ''
+                    ? ''
+                    : cx(
+                        'calendar-day',
+                        `${
+                          day === new Date().getDate() &&
+                          month === new Date().getMonth() &&
+                          year === new Date().getFullYear()
+                            ? 'curr-date'
+                            : ''
+                        }`,
+                      )
+                }
               >
                 {day}
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
               </div>
             ))}
           </div>
         </div>
+        {showMonthPicker && (
+          <div className={cx('month-list')}>
+            {month_names.map((month_name, index) => (
+              <div
+                className={cx('month-list-item')}
+                onClick={() => {
+                  setMonth(index);
+                  setShowMonthPicker(false);
+                }}
+              >
+                <div className={cx('month-name')}>{month_name}</div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
