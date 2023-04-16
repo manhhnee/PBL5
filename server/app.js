60613f8f0d948544f8a25a078f73b4a1102b58bf
darknesshaotran <darknessclmm@gmail.com>
2023-04-16 19:16:07 +0700

    .

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors')
 const cookie_parser = require('cookie-parser')
 const route = require('./routes/index')
+const bodyParser = require("body-parser");
 const app = express();
 
-const port = 3000
+const port = 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cors())
 app.use(cookie_parser())
