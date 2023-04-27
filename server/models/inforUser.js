8e1dfbb253af29c2fbaa51efe53438c076662f3b
darknesshaotran <84674265+darknesshaotran@users.noreply.github.com>
2023-04-27 21:40:28 +0700

    Update inforUser.js

--- server/models/inforUser.js
+++ server/models/inforUser.js
@@ -1,29 +1,5 @@
 const db = require("../config/db/index");
 
-<<<<<<< HEAD
-const inforUser = function (inforUser) {
-  (this.id = inforUser.id),
-    (this.id_Account = inforUser.id_Account),
-    (this.FirstName = inforUser.FirstName),
-    (this.LastName = inforUser.LastName),
-    (this.PhoneNumber = inforUser.PhoneNumber),
-    (this.Address = inforUser.Address),
-    (this.Avatar = inforUser.Avatar);
-};
-inforUser.findWithAccountId = function (idAccount, results) {
-  db.query(
-    "SELECT * FROM inforuser WHERE id_Account = ?",
-    idAccount,
-    (err, infor) => {
-      if (err) throw err;
-      results(infor[0]);
-    }
-  );
-};
-module.exports = inforUser;
-=======
-
-
 const inforUser = function(inforUser){
     this.id = inforUser.id,
     this.id_Account = inforUser.id_Account,
@@ -80,4 +56,4 @@ inforUser.update = function(idAccount,avatarPath,data,results)
     }
 }
 module.exports = inforUser
->>>>>>> hao
+
