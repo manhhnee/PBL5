70ab4554f324f9799d271e0e0f947cf2d921b5a6
darknesshaotran <darknessclmm@gmail.com>
2023-06-06 14:25:07 +0700

    change password method

--- server/routes/account.js
+++ server/routes/account.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const AccountController = require("../controllers/account");
+const AuthController = require("../controllers/auth");
 var router = express.Router();
-// var AuthController = require('../controllers/auth')
 
 // router.get('/URL',itemsController.method)
 
 router.post("/register", AccountController.register);
 router.post("/login", AccountController.login);
-router.post("/", (req, res, next) => {
-  console.log(req.body.name);
-  res.json(req.body.name);
-});
+router.put("/changePass",AuthController.verifyToken,AccountController.changePassword)
 //  router.get('/private',AuthController.verifyToken,AuthController.isCustomer,AccountController.getAccount)
 
 module.exports = router;
